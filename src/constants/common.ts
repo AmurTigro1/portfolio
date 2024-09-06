8d0a86d1328614b247ce53e799647e0573f6f26a
Stern <sternsalera878@gmail.com>
2024-09-06 08:13:22 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Josephen Stern Salera';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Detail-oriented and highly motivated 4th-year Information Technology student at Mater Dei College, seeking an entry-level position or internship in the IT industry. Eager to apply technical skills, creativity, and problem-solving abilities to real-world challenges.';
+export const ABOUT_TEXT_2 =
+  'Enthusiastic 4th-year Information Technology student at Mater Dei College with a strong foundation in software development and network administration. Seeking an internship to leverage my technical skills and academic knowledge in a dynamic and growth-oriented environment.';
+export const ABOUT_TEXT_3 =
+  'Enthusiastic and detail-oriented Information Technology graduate from Mater Dei College, seeking an entry-level position where I can leverage my technical skills and passion for innovation to contribute to the growth and success of the company.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Developed a web-based application for a boarding house reservation system using Laravel and MySQL.';
+export const RESUME_TEXT_2 =
+  'Implemented user authentication, real-time notifications, and dynamic search functionality.';
